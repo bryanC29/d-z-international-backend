1314d889ab22358a7ae93bd128117f4e6394819f
Bryan Christy <bryanchristy2016@gmail.com>
2025-03-19 23:35:51 +0530

    ADD: Mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,13 +5,20 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostgreSQLConfig } from './common/config/pg.config';
 import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { MongooseConfig } from './common/config/mongo.config';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
       useFactory: () => PostgreSQLConfig,
     }),
+    MongooseModule.forRootAsync({
+      useFactory: () => MongooseConfig,
+    }),
     AuthModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/common/config/mongo.config.ts
@@ -0,0 +1,5 @@
+/* eslint-disable prettier/prettier */
+
+export const MongooseConfig = {
+  uri: 'mongodb://localhost:27017/d_z_international',
+};
