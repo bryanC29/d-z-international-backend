0f9701a09261ac9d7e6013c7a72799d5a991192b
Bryan Christy <bryanchristy2016@gmail.com>
2025-03-29 11:49:31 +0530

    BUILD: Return entity

---
+++ src/common/entity/return.entity.ts
@@ -0,0 +1,54 @@
+/* eslint-disable prettier/prettier */
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+export enum ReturnStatus {
+  PENDING = 'pending',
+  ACCEPTED = 'accepted',
+  REJECTED = 'rejected',
+}
+
+export enum ReturnTrackingStatus {
+  PENDING = 'pending',
+  PICKED = 'picked',
+  PROCESSING = 'processing',
+  DECLINED = 'declined',
+  REFUNDED = 'refunded',
+}
+
+@Entity()
+export class Return {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    type: 'varchar',
+    nullable: false,
+  })
+  uid: string;
+
+  @Column({
+    type: 'varchar',
+    nullable: false,
+  })
+  orderId: string;
+
+  @Column({
+    type: 'varchar',
+    nullable: false,
+  })
+  productItemId: string;
+
+  @Column({
+    type: 'enum',
+    enum: ReturnStatus,
+    default: ReturnStatus.PENDING,
+  })
+  status: string;
+
+  @Column({
+    type: 'enum',
+    enum: ReturnTrackingStatus,
+    default: ReturnTrackingStatus.PENDING,
+  })
+  trackingStatus: string;
+}
