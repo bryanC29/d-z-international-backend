09d131e42c8c28b475a108e0b99d100a9ec543ef
Bryan Christy <bryanchristy2016@gmail.com>
2025-04-05 00:17:10 +0530

    FIX: Production DB conn

--- src/common/config/pg.config.ts
+++ src/common/config/pg.config.ts
@@ -6,17 +6,29 @@ import { ConfigService } from '@nestjs/config';
 export const PostgreSQLConfig = (
   configService: ConfigService,
 ): Promise<TypeOrmModuleOptions> => {
-  const config: TypeOrmModuleOptions = {
+  const isProduction = configService.get<string>('PRODUCTION') == 'true';
+
+  let config: TypeOrmModuleOptions = {
     type: 'postgres',
     host: configService.get<string>('DB_HOST'),
     port: configService.get<number>('DB_PORT'),
     username: configService.get<string>('DB_USERNAME'),
     password: configService.get<string>('DB_PASSWORD'),
     database: configService.get<string>('DB_DATABASE'),
-    entities: [path.join(__dirname, '**/*.entity.{ts,js}')],
+    entities: [path.join(__dirname, '../../', '**', '*.entity{.ts,.js}')],
     synchronize: configService.get<string>('PRODUCTION') == 'false',
     logging: configService.get<string>('PRODUCTION') == 'false',
   };
+
+  if (isProduction) {
+    config = {
+      ...config,
+      ssl: {
+        rejectUnauthorized: false,
+        ca: configService.get<string>('DB_CA_CERT'),
+      },
+    };
+  }
 
   return Promise.resolve(config);
 };
