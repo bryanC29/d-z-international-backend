5269611e8e512a1932ba9db5f18d4d21384387e6
Bryan Christy <bryanchristy2016@gmail.com>
2025-04-05 15:01:06 +0530

    FIX: Production jwt secret management

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -25,7 +25,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
         return token;
       },
       ignoreExpiration: false,
-      secretOrKey: process.env.JWT_SECRET || 'secret',
+      secretOrKey: configService.get<string>('JWT_SECRET') || 'secret',
     });
   }
 
