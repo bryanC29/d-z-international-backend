7e02d36e81e0a5b7df11413f32d38790de50239d
Bryan Christy <bryanchristy2016@gmail.com>
2025-04-12 15:44:42 +0530

    RFC: Order logic

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -3,9 +3,10 @@ import { OrderController } from './order.controller';
 import { OrderService } from './order.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Order } from 'src/common/entity/order.entity';
+import { OrderItem } from 'src/common/entity/orderItem.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Order])],
+  imports: [TypeOrmModule.forFeature([Order, OrderItem])],
   controllers: [OrderController],
   providers: [OrderService],
 })
--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -3,6 +3,7 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
 import { Order } from 'src/common/entity/order.entity';
+import { OrderItem } from 'src/common/entity/orderItem.entity';
 import { Repository } from 'typeorm';
 
 @Injectable()
@@ -10,6 +11,9 @@ export class OrderService {
   constructor(
     @InjectRepository(Order)
     private readonly orderRepository: Repository<Order>,
+
+    @InjectRepository(OrderItem)
+    private readonly orderItemRepository: Repository<OrderItem>,
   ) {}
 
   async getAllOrders(paginationDto: PaginationDto) {
@@ -31,9 +35,18 @@ export class OrderService {
   }
 
   async getOrderById(id: number) {
-    const order = await this.orderRepository.findOne({ where: { id } });
+    const orderData = await this.orderRepository.findOne({ where: { id } });
+    if (!orderData) throw new NotFoundException('Order not found!');
 
-    if (!order) throw new NotFoundException('Order not found!');
+    const orderItemData = await this.orderItemRepository.find({
+      where: { order_id: orderData.id },
+    });
+    if (!orderItemData) throw new NotFoundException('Order item not found!');
+
+    const order = {
+      orderData,
+      orderItem: orderItemData,
+    };
 
     return order;
   }
