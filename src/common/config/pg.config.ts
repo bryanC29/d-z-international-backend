91596a35cede53cc0cf457e30c1d5eb8620e0d50
Bryan Christy <bryanchristy2016@gmail.com>
2025-05-15 00:02:30 +0530

    UPDATE: PG Conn config

--- src/common/config/pg.config.ts
+++ src/common/config/pg.config.ts
@@ -6,28 +6,23 @@ import { ConfigService } from '@nestjs/config';
 export const PostgreSQLConfig = (
   configService: ConfigService,
 ): Promise<TypeOrmModuleOptions> => {
-  const isTesting = configService.get<string>('NODE_ENV') == 'test';
+  const isTesting = configService.get<string>('NODE_ENV') === 'test';
 
-  let config: TypeOrmModuleOptions = {
+  const config: TypeOrmModuleOptions = {
     type: 'postgres',
     host: configService.get<string>('DB_HOST'),
-    port: configService.get<number>('DB_PORT'),
+    port: parseInt(configService.get<string>('DB_PORT') || '5432', 10),
     username: configService.get<string>('DB_USERNAME'),
     password: configService.get<string>('DB_PASSWORD'),
     database: configService.get<string>('DB_DATABASE'),
     entities: [path.join(__dirname, '../../', '**', '*.entity{.ts,.js}')],
-    synchronize: configService.get<string>('NODE_ENV') != 'production',
-    logging: configService.get<string>('NODE_ENV') != 'production',
+    synchronize: configService.get<string>('NODE_ENV') !== 'production',
+    logging: configService.get<string>('NODE_ENV') !== 'production',
+    ssl:
+      isTesting || configService.get<string>('DB_SSL') === 'true'
+        ? { rejectUnauthorized: false }
+        : undefined,
   };
-
-  if (isTesting) {
-    config = {
-      ...config,
-      ssl: {
-        rejectUnauthorized: false,
-      },
-    };
-  }
 
   return Promise.resolve(config);
 };
